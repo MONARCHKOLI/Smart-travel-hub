aba03d14be9eb460665c40c330f20e9ae121e587
MONARCHKOLI <monarchkoli12@gmail.com>
2026-05-07 15:00:41 +0530

    Auth: Verified Login/Logout and Lockout logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,22 @@
 Rails.application.routes.draw do
-  root "home#index"
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: "users/sessions"
+  }
+  
+  authenticated :user do
+    root "home#index", as: :authenticated_root
+  end
+
+  unauthenticated do
+    root "devise/sessions#new", as: :unauthenticated_root
+  end
+
   resources :food_orders
   resources :food_items
   resources :bookings
   resources :rooms
   resources :motels
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  # Health check
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
