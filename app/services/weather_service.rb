5e2697eb5c957360a56645f19d4f489b5f0c0445
MONARCHKOLI <monarchkoli12@gmail.com>
2026-05-22 13:51:24 +0530

    Updated ENV for render for weather api

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -1,9 +1,11 @@
-require 'net/http'
-require 'json'
+require "net/http"
+require "json"
 
 class WeatherService
   def self.get_weather(lat, lng)
-    api_key = Rails.application.credentials.stormglass[:api_key]
+    api_key = ENV["STORMGLASS_API_KEY"]
+
+    return nil if api_key.blank?
 
     url = URI(
       "https://api.stormglass.io/v2/weather/point?lat=#{lat}&lng=#{lng}&params=airTemperature"
@@ -13,11 +15,17 @@ class WeatherService
     http.use_ssl = true
 
     request = Net::HTTP::Get.new(url)
-
     request["Authorization"] = api_key
 
     response = http.request(request)
 
-    JSON.parse(response.body)
+    data = JSON.parse(response.body)
+
+    Rails.logger.info(data.inspect)
+
+    data
+  rescue => e
+    Rails.logger.error("Weather Error: #{e.message}")
+    nil
   end
 end
